7151603605dd3d5e51f8ca2bee0c359cbc4d8bc0
BAEJUN SON <78029066+sonbaejun@users.noreply.github.com>
2022-10-05 16:20:33 +0900

    Delete ProgramingLanguage directory

--- ProgramingLanguage/hello.fs
+++
@@ -1,8 +0,0 @@
-let double x = 2 * x
-let add1 x = x + 1
-let zero = 0
-let my = (double (double (add1 (double (add1 (double (add1 (double (add1 (double (add1
-(double (add1 (double (add1 (double (add1 (double (double (add1 zero))))))))))))))))))))
-let my2 = (double (add1 (double (double (double (double (add1 (double (add1
-(double (add1 (double (add1 (double (add1 (double (add1 zero)))))))))))))))))
-printfn "%d%d"  my2 my;;
